781ef17e1faf677cb0ca82a2d1cbc2a9f9f118d1
Chaston Harper <chastonharper1@gmail.com>
2020-05-27 18:21:02 -0500

    Made it so that the lower keyboard container is hidden when the page loads

---
+++ app.js
@@ -0,0 +1,13 @@
+let upperKeys = ("#keyboard-upper-container");
+let lowerKeys = ("#keyboard-lower-container");
+let sentences = [
+    "ten ate neite ate nee enet ite ate inet ent eate",
+    "Too ato too nOt enot one totA not anot tOO aNot",
+    "oat itain oat tain nate eate tea anne inant nean",
+    "itant eate anot eat nato inate eat anot tain eat",
+    "nee ene ate ite tent tiet ent ine ene ete ene ate"
+];
+
+$(document).ready(function() {
+    $("#keyboard-lower-container").hide();
+});
