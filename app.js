34b40cc8e6c81ee2d1ac475ac18795fda73529bb
Chaston Harper <chastonharper1@gmail.com>
2020-05-29 10:16:18 -0500

    final code

--- app.js
+++ app.js
@@ -37,3 +37,64 @@ $(document).keypress(function(e) {
         $(key).css("background-color","");
     });
 });
+
+//Sentences
+
+let array = 0;
+let writing = sentences[array];
+let currentKey = 0;
+let letter = writing.substring(currentKey, currentKey+1)
+$("#sentence").text(writing);
+$("#target-letter").text(letter);
+let timerOn = false;
+let startTime;
+let errors = 0;
+
+$(document).keypress(function(e) {
+    if (timerOn === false) {
+      startDate = new Date();
+      startTime = startDate.getTime();
+      timerOn = true;
+    }
+
+    if (e.which == sentences[array].charCodeAt(currentKey)) {
+        let correct = $('<span class="green">✓</span>');
+        $(correct).appendTo("#feedback");
+        
+        $("#yellow-block").css("left", "+=17.3px");
+        
+        currentKey++;
+        letter = writing.substring(currentKey,currentKey + 1);
+        $("#target-letter").text(letter);
+       
+        if (currentKey === writing.length) {
+          array++; 
+          //if sentences are complete
+          if (array === sentences.length) {
+            let endDate = new Date();
+            let endTime = endDate.getTime();
+            let minutes = (endTime - startTime) / 60000;
+            wpm = Math.round(54 / minutes - 2 * errors);
+            var confirmBox = confirm(
+              `You typed ${wpm} words per minute! Would you like to try again?`
+            );
+            if (confirmBox == true) {
+              location.reload();
+            }
+          } else {
+           
+            writing = sentences[array];
+            $("#sentence").text(writing);
+            currentKey = 0;
+            letter = writing.substring(currentKey, currentKey + 1);
+            $("#target-letter").text(letter);
+            $("#yellow-block").css("left", "15px");
+            $("#feedback").text("");
+          }
+        }
+      } else {
+        let wrong = $('<span class="red">x</span>');
+        $(wrong).appendTo("#feedback");
+        errors++;
+      }
+    });
